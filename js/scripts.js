89dc708bf118620a5564f03d6652d8386dec498c
Spectre <mikhail.poboinya@ya.ru>
2024-12-13 08:12:13 +0800

    Layers system implemented

--- js/scripts.js
+++ js/scripts.js
@@ -3,38 +3,22 @@ const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engi
 var sphere;
 var delta = 0;
 
-var paintmode = 1;
-var field = [
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-]
+const CANVASHEIGHT = 768;
+const CANVASWIDTH = 768;
 
-const createChessBoard = function(posX, posY, posZ, size, squares = 3) {
-    for (let i = -squares+1; i < squares; i++){
-        for (let j = -squares+1; j < squares; j++){
-            let square = BABYLON.MeshBuilder.CreateGround("chess"+i+j, {width:size, height:size}, scene)
-            let material = new BABYLON.StandardMaterial(scene);
-            material.alpha = 1;
+const FIELDSIZE = 16;
+const LAYERSAMOUNT = 5;
 
-            if ((i+j)%2==0){
-                material.diffuseColor = new BABYLON.Color3.White;
-            } else {
-                material.diffuseColor = new BABYLON.Color3.Gray;
-            }
-            square.position.x = i*size + posX;
-            square.position.y = posY;
-            square.position.z = j*size + posZ;
+var PAINTMODE = 1;
+var CURRENTLAYER = 0;
 
-            square.material = material;
-        }
+var mapData = new Array(LAYERSAMOUNT)
+for (let i = 0; i < LAYERSAMOUNT; i++){
+    mapData[i] = new Array(FIELDSIZE);
+    for (let j = 0; j < FIELDSIZE; j++){
+        mapData[i][j] = new Array(FIELDSIZE).fill(0);
     }
+
 }
 
 const createScene = function () {
@@ -45,13 +29,14 @@ const createScene = function () {
     //camera.attachControl(canvas, true);
     camera.rotation.y = 0;
     camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
-    camera.orthoTop = 5;
-    camera.orthoBottom = -5;
-    camera.orthoRight = 5;
-    camera.orthoLeft = -5;
+    camera.orthoTop = FIELDSIZE/2;
+    camera.orthoBottom = -FIELDSIZE/2;
+    camera.orthoRight = FIELDSIZE/2;
+    camera.orthoLeft = -FIELDSIZE/2;
     
     const light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0, -1, 0), scene);
-    light.intensity = 0.5;
+    light.intensity = 1;
+    light.specular = new BABYLON.Color3(0,0,0);
 
     sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 1, segments: 32}, scene);
     sphere.position.y = 1;
@@ -59,64 +44,76 @@ const createScene = function () {
     return scene;
 };
 
-const createTile = function(posX, posY){
-    let plate = BABYLON.MeshBuilder.CreateGround("plateX"+posX+"Y"+posY, {width:1, height:1}, scene)
-    plate.position.x = posX;
-    plate.position.z = posY;
-    plate.position.y = 0;
+const createChessBoard = function(posX, posY, posZ, size, squares, scene) {
+    for (let i = 0; i < squares; i++){
+        for (let j = 0; j < squares; j++){
+            let square = BABYLON.MeshBuilder.CreateGround("chess"+i+j, {width:size, height:size}, scene)
+            let material = new BABYLON.StandardMaterial(scene);
+            material.alpha = 1;
+
+            if ((i+j)%2==0){
+                material.diffuseColor = new BABYLON.Color3.White;
+            } else {
+                material.diffuseColor = new BABYLON.Color3.Gray;
+            }
+            square.position.x = (i-squares/2+0.5)*size + posX;
+            square.position.y = posY;
+            square.position.z = (j-squares/2+0.5)*size + posZ;
+
+            square.material = material;
+        }
+    }
+}
+
+const createTile = function(posX, posY, height){
+    if (scene.getMeshByName("tileX"+posX+"Y"+posY+"level"+CURRENTLAYER)){
+        return null
+    }
+    let tile = BABYLON.MeshBuilder.CreateGround("tileX"+posX+"Y"+posY+"level"+CURRENTLAYER, {width:1, height:1}, scene)
+    tile.position.x = posX;
+    tile.position.z = posY;
+    tile.position.y = height;
 
     let material = new BABYLON.StandardMaterial(scene);
     material.alpha = 1;
-    material.diffuseColor = new BABYLON.Color3.Teal;
+    material.diffuseColor = new BABYLON.Color3(Math.min(1, posX/FIELDSIZE + 0.5), Math.min(1, posY/FIELDSIZE + 0.5), Math.min(1, height/LAYERSAMOUNT))
 
-    plate.material = material;
+    tile.material = material;
 
-    return plate;
+    return tile;
 }
 
-const scene = createScene(); //Call the createScene function
+const moveTarget = function(target, horizontalDistance, verticalDistance){
+    target.position.x += horizontalDistance;
+    target.position.z += verticalDistance;
+}
 
-console.log(scene, "Scene");
-console.log(sphere, "Sphere");
-
-createChessBoard(0,-1,0,1,5);
 engine.runRenderLoop(function () {
-        sphere.position.y = 1 + Math.sin(delta*0.05)*0.2;
-        delta += 1;
-        scene.render();
+    sphere.position.y = 1 + Math.sin(delta*0.05)*0.2;
+    delta += 1;
+    scene.render();
 });
+
 window.addEventListener("resize", function () {
-        engine.resize();
+    engine.resize();
 });
-const moveLeft = function(target){
-    target.position.x += 1;
-}
-const moveRight = function(target){
-    target.position.x -= 1;
-}
-const moveUp = function(target){
-    target.position.z += 1;
-}
-const moveDown = function(target){
-    target.position.z -= 1;
-}
+
+const scene = createScene(); 
+console.log(scene)
+//createChessBoard(0, -20, 0, 1, FIELDSIZE, scene);
 
 canvas.addEventListener("click", function(event){
     let x = event.x;
     let y = event.y;
-    if (x > 38 & x < 730 & y > 38 & y < 730){
-        x -= 38;
-        y -= 38;
-        x = Math.floor(x/76.8888)
-        y = Math.floor(y/76.8888)
-        let xTranslated = x - 4
-        let yTranslated = 8 - y -4
-        if (event.altKey){
-            scene.removeMesh(scene.getMeshByName("plateX"+xTranslated+"Y"+yTranslated))
-            field[x][y] = 0;
-        } else {
-            createTile(xTranslated,yTranslated);
-            field[x][y] = paintmode;
-        }
+    x = Math.floor(x/(CANVASHEIGHT/FIELDSIZE))
+    y = Math.floor(y/(CANVASHEIGHT/FIELDSIZE))
+    let xTranslated = x - FIELDSIZE/2 + 0.5
+    let yTranslated = FIELDSIZE - y - FIELDSIZE/2 - 0.5
+    if (event.altKey){
+        scene.removeMesh(scene.getMeshByName("tileX"+xTranslated+"Y"+yTranslated+"level"+CURRENTLAYER))
+        mapData[CURRENTLAYER][y][x] = 0;
+    } else {
+        createTile(xTranslated,yTranslated, CURRENTLAYER);
+        mapData[CURRENTLAYER][y][x] = PAINTMODE;
     }
 })
