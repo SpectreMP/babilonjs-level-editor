5585300abbde943135dffbfb0df4511b28e45a86
Spectre <mikhail.poboinya@ya.ru>
2024-12-16 08:24:45 +0800

    whatever

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engi
 var sphere;
 var delta = 0;
 
+
+
 const CANVASHEIGHT = 768;
 const CANVASWIDTH = 768;
 
@@ -13,14 +15,24 @@ var PAINTMODE = 1;
 var MATERIALSLIST = [];
 var CURRENTLAYER = 0;
 
+const materialButton = document.querySelector("#Material")
+const layerButton = document.querySelector("#Layer")
+
 var WINCONDITION = false;
 
+var EXAMPLEMAP = [];
+EXAMPLEMAP = [[["2","2","2","2","2","2","2","2","2","2","2","2","2","2","2","2"],["2",0,0,0,0,0,0,0,0,0,0,0,0,0,0,"2"],["2",0,0,0,0,0,0,0,0,0,0,0,0,0,0,"2"],["2","2","2","2","1","1","1","1","1","1","1","2",0,0,0,"2"],["3","2","2","2","1","1","1","1","1","1","1","2",0,0,0,"2"],["3","3","2","2","1","1","1","1","1","1","1","2",0,0,0,"2"],["3","3","3","2","1","1","1","1","1","1","1","2",0,0,0,"2"],["3","3","3","2","2","2","1","1","1","1","1","2",0,0,0,"2"],["3","3","3","3","2","2","1","1","1","1","1","2",0,0,0,"2"],["3","3","3","3","2","2","2","1","1","1","2","2","2",0,0,"2"],["3","3","3","3","3","3","2","2","2","2","2","3","2",0,0,"2"],["3","3","3","3","3","3","3","3","3","3","3","3","2","2",0,"2"],["3","3","3","3","3","3","3","3","3","3","3","3","2","2","2","2"],["3","3","3","3","3","3","3","3","3","3","3","3","3","2","2","2"],["3","3","3","3","3","3","3","3","3","3","3","3","3","3","2","2"],["3","3","3","3","3","3","3","3","3","3","3","3","3","3","3","2"]],[["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"],["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"],["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"],[0,0,0,"1","1","1","1","1","1","1","1","1","1","1","1","1"],[0,0,0,0,"1","1","1","1","1","1","1","1","1","1","1","1"],[0,0,0,0,"1","1","1","1","1","1","1","1","1","1","1","1"],[0,0,0,0,"1","1","1","1","1","1","1",0,"1","1","1","1"],[0,0,0,0,0,0,"1","1","1","1","1",0,"1","1","1","1"],[0,0,0,0,0,0,"1","1","1","1","1",0,"1","1","1","1"],[0,0,0,0,0,0,0,"1","1","1",0,0,0,"1","1","1"],[0,0,0,0,0,0,0,0,0,0,"4",0,0,"1","1","1"],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,"1","1"],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"1"],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"5"],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]]
 var MAPDATA = new Array(LAYERSAMOUNT)
 for (let i = 0; i < LAYERSAMOUNT; i++){
     MAPDATA[i] = new Array(FIELDSIZE);
     for (let j = 0; j < FIELDSIZE; j++){
         MAPDATA[i][j] = new Array(FIELDSIZE).fill(0);
     }
+}
+
+const reloadParams = function(){
+    PAINTMODE = materialButton.value;
+    CURRENTLAYER = layerButton.value;
 }
 
 const initializeMaterials = function(scene){
@@ -36,7 +48,7 @@ const initializeMaterials = function(scene){
     //Red material
     material = new BABYLON.StandardMaterial(scene)
     material.alpha = 1;
-    material.diffuseColor = new BABYLON.Color3.Red;
+    material.diffuseColor = new BABYLON.Color3.Yellow;
     MATERIALSLIST.push(material);
 
     //Blue material
@@ -126,15 +138,15 @@ const switchCameras = function(camera1, camera2, scene){
 }
 
 const createTile = function(posX, posY, height, materialNumber, scene){
-    if (MAPDATA[CURRENTLAYER][posY][posX] == materialNumber){
+    if (MAPDATA[height][posY][posX] == materialNumber){
         return null;
     }
     if (materialNumber == 0){
-        MAPDATA[CURRENTLAYER][posY][posX] = materialNumber;
-        scene.removeMesh(scene.getMeshByName("tileX"+posX+"Y"+posY+"level"+CURRENTLAYER))
+        MAPDATA[height][posY][posX] = materialNumber;
+        scene.removeMesh(scene.getMeshByName("tileX"+posX+"Y"+posY+"level"+height))
         return null;
     } else {
-        if (MAPDATA[CURRENTLAYER][posY][posX] != materialNumber){
+        if (MAPDATA[height][posY][posX] != materialNumber){
             createTile(posX, posY, height, 0, scene);
         }
         if (materialNumber == 4){
@@ -147,7 +159,7 @@ const createTile = function(posX, posY, height, materialNumber, scene){
                     }
                 }
             }
-            MAPDATA[CURRENTLAYER][posY][posX] = materialNumber;
+            MAPDATA[height][posY][posX] = materialNumber;
             return initializeExit(posX, FIELDSIZE - posY, height, scene);
         }
         if (materialNumber == 5){
@@ -160,11 +172,11 @@ const createTile = function(posX, posY, height, materialNumber, scene){
                     }
                 }
             }
-            MAPDATA[CURRENTLAYER][posY][posX] = materialNumber;
+            MAPDATA[height][posY][posX] = materialNumber;
             return initializePlayer(posX, FIELDSIZE - posY, height, scene);
         }
-        MAPDATA[CURRENTLAYER][posY][posX] = materialNumber;
-        let tile = BABYLON.MeshBuilder.CreateBox("tileX"+posX+"Y"+posY+"level"+CURRENTLAYER, {size:1}, scene)
+        MAPDATA[height][posY][posX] = materialNumber;
+        let tile = BABYLON.MeshBuilder.CreateBox("tileX"+posX+"Y"+posY+"level"+height, {size:1}, scene)
         tile.position.x = posX+0.5;
         tile.position.z = FIELDSIZE - posY-0.5;
         tile.position.y = height;
@@ -278,3 +290,25 @@ canvas.addEventListener("keydown", function(event){
     }
 
 })
+
+const exportMap = function(){
+    let jsonString = JSON.stringify(MAPDATA);
+    function download(content, fileName, contentType) {
+        var a = document.createElement("a");
+        var file = new Blob([content], {type: contentType});
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.click();
+    }
+    download(jsonString, 'exampleMap.json', 'text/plain');
+    console.log(jsonString);
+}
+const importMap = function(map){
+    for (let i = 0; i < LAYERSAMOUNT; i++){
+        for (let j = 0; j < FIELDSIZE; j++){
+            for (let k = 0; k < FIELDSIZE; k++){
+                createTile(k,j,i,map[i][j][k], scene);
+            }
+        }
+    }
+}
